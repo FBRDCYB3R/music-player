381b4d44841eb60f9f51dd935e935c78b60bc1ac
Bak David <bak.david96@gmail.com>
2021-09-20 19:36:45 +0300

    Added randomize to music, moved everything into the right folder

--- script.js
+++ script.js
@@ -12,40 +12,42 @@ const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
 // Song titles
-const songs = ['Armand Van Helden - My My My (DJ KUBA & NEITAN Remix).mp3',
-'Bassjackers ft. Luciana - Fireflies (Official Music Video).mp3',
-'Catiso - Zanobi.mp3',
-'Cazzette - Blind Heart (Broiler Remix).mp3',
-'David Guetta, Martin Garrix & Brooks - Like I Do (Lyric Video).mp3',
-'Dax - !Homicide! Freestyle [One Take Video].mp3',
-'Doja Cat - Boss B!tch (from Birds of Prey! The Album) [Official Music Video].mp3',
-'Dropgun - Drought ft. Nevve (Official Audio).mp3',
-'Dropgun - Tomorrow Never Comes (feat. Bryan Finlay).mp3',
-'Every Little Thing (Bazzflow Remix Radio).mp3',
-"Florian Picasso - Final Call (Mesto & Justin Mylo Remix).mp3",
-'Jay Eskar - Awakening.mp3',
-'Jay Eskar - Chakra.mp3',
-'Logic - Everybody (Lyrics).mp3',
-'Masked Wolf - Astronaut In The Ocean (Ozlig Remix).mp3',
-'Metrik - Gravity.mp3',
-'Mike Williams - Konnichiwa (Original Mix).mp3',
-'MR BLACK & Offer Nissim - Mucho Bien (MR.BLACK Remix).mp3',
-'Netsky - Rio.mp3',
-'New Workout Music Motivation Playlist 2018.mp3',
-'NF - When I Grow Up.mp3',
-'Phoebe Ryan - Mine (Elephante Remix).mp3',
-'Queen Of A Lonely Heart (feat. Lourdiz) (Dastic x Robbie Mendez Club Mix).mp3',
-'RIVERO & Triangle - WICKD (feat. Dean) [Official Audio].mp3',
-'RYLLZ - I Gotta Know.mp3',
-'SHAHMEN - MARK (EMR3YGUL Remix).mp3',
-'Shahmen - Mark.mp3',
-'SHAHMEN - MARK EMR3YGUL Remix INFINITY BASS #enjoybeauty.mp3',
-'Steve Aoki - Back to Earth feat. Fall Out Boy (LA Riots Remix).mp3',
-"The Black Eyed Peas - Let's Get It Started (Galwaro Remix).mp3",
-'Timmy Trumpet Freaks (Radio Edit).mp3',
-'Tupac  - Changes (Dax Remix) [One Take].mp3',
-'WATEVA - Ber Zer Ker (Rob Gasser Remix) [NCS Release].mp3'
+const songs = ['Armand Van Helden - My My My (DJ KUBA & NEITAN Remix)',
+'Bassjackers ft. Luciana - Fireflies (Official Music Video)',
+'Catiso - Zanobi',
+'Cazzette - Blind Heart (Broiler Remix)',
+'David Guetta, Martin Garrix & Brooks - Like I Do (Lyric Video)',
+'Dax - !Homicide! Freestyle [One Take Video]',
+'Doja Cat - Boss B!tch (from Birds of Prey! The Album) [Official Music Video]',
+'Dropgun - Drought ft. Nevve (Official Audio)',
+'Dropgun - Tomorrow Never Comes (feat. Bryan Finlay)',
+'Every Little Thing (Bazzflow Remix Radio)',
+"Florian Picasso - Final Call (Mesto & Justin Mylo Remix)",
+'Jay Eskar - Awakening',
+'Jay Eskar - Chakra',
+'Logic - Everybody (Lyrics)',
+'Masked Wolf - Astronaut In The Ocean (Ozlig Remix)',
+'Metrik - Gravity',
+'Mike Williams - Konnichiwa (Original Mix)',
+'MR BLACK & Offer Nissim - Mucho Bien (MR.BLACK Remix)',
+'Netsky - Rio',
+'New Workout Music Motivation Playlist 2018',
+'NF - When I Grow Up',
+'Phoebe Ryan - Mine (Elephante Remix)',
+'Queen Of A Lonely Heart (feat. Lourdiz) (Dastic x Robbie Mendez Club Mix)',
+'RIVERO & Triangle - WICKD (feat. Dean) [Official Audio]',
+'RYLLZ - I Gotta Know',
+'SHAHMEN - MARK (EMR3YGUL Remix)',
+'Shahmen - Mark',
+'SHAHMEN - MARK EMR3YGUL Remix INFINITY BASS #enjoybeauty',
+'Steve Aoki - Back to Earth feat. Fall Out Boy (LA Riots Remix)',
+"The Black Eyed Peas - Let's Get It Started (Galwaro Remix)",
+'Timmy Trumpet Freaks (Radio Edit)',
+'Tupac  - Changes (Dax Remix) [One Take]',
+'WATEVA - Ber Zer Ker (Rob Gasser Remix) [NCS Release]'
 ];
+
+shuffle(songs);
 
 // Keep track of song
 let songIndex = 0;
@@ -53,10 +55,29 @@ let songIndex = 0;
 // Initially load song details into DOM
 loadSong(songs[songIndex]);
 
+//randomize array elements function
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 // Update song details
 function loadSong(song) {
   title.innerText = song;
-  audio.src = `music/${song}`;
+  audio.src = `music/${song}.mp3`;
   cover.src = `images/workout.jpg`;
 }
 
